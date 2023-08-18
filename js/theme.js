0c1d14af72b930383a7d95eaf4b79572079d9186
Davinder Singh <44398533+davinderdhindsa350@users.noreply.github.com>
2023-08-19 00:11:16 +0530

    header changes

--- js/theme.js
+++ js/theme.js
@@ -7,18 +7,18 @@
 	-------------------------------------------------------------------------------*/
 
 	//* Navbar Fixed  
-    function navbarFixed(){
-        if ( $('.main_menu_area').length ){ 
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
-                if (scroll >= nav_offset_top ) {
-                    $(".main_menu_area").addClass("navbar_fixed");
-                } else {
-                    $(".main_menu_area").removeClass("navbar_fixed");
-                }
-            });
-        };
-    };
+    // function navbarFixed(){
+    //     if ( $('.main_menu_area').length ){ 
+    //         $(window).scroll(function() {
+    //             var scroll = $(window).scrollTop();   
+    //             if (scroll >= nav_offset_top ) {
+    //                 $(".main_menu_area").addClass("navbar_fixed");
+    //             } else {
+    //                 $(".main_menu_area").removeClass("navbar_fixed");
+    //             }
+    //         });
+    //     };
+    // };
     // navbarFixed();
     
     
